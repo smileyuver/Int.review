180313f7b4ecec482f0405b636eb6a097083ac39
smileyuver <im.smileyuver@gmail.com>
2017-04-07 21:57:17 +0900

    All the set ups for basic functions are done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,15 @@ class ReviewsController < RankingController
   end
 
   def create
+    binding.pry
     # Review.create(create_params)
+    Review.create(create_params)
     # トップページにリダイレクトする
+    redirect_to controller: :products, action: :index
   end
 
   private
   def create_params
+    params.require(:review).permit(:nickname, :rate, :review).merge(product_id: params[:product_id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406132050) do
+ActiveRecord::Schema.define(version: 20170407074349) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170406132050) do
     t.integer  "director",   limit: 4
     t.text     "detail",     limit: 65535
     t.string   "open_data",  limit: 255
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "nickname",   limit: 255
+    t.integer  "rate",       limit: 4
+    t.text     "review",     limit: 65535
+    t.integer  "product_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
